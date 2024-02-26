6bd4bb91b76bdf0dd531b1fa30fb9799f8a39708
adnan34k <2022bit055@sggs.ac.in>
2024-02-26 16:20:59 +0530

    update

--- Practical8/library.java
+++ Practical8/library.java
@@ -1,78 +1,114 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-class Library{
-	static ArrayList<String> booklist = new ArrayList<String>(); 
-	public static void main(String[] args){
-		
-		System.out.println("Welcome To Library");
-		System.out.println("Please Select the option");
-		System.out.println("1.Borrow book\n2.Return book\n3.Add new book in library\n4.Exit");
-		int i;
-		Scanner s1 = new Scanner(System.in);
-		do{
-		
-		i = s1.nextInt();
-		switch(i){
-		case 1:User.borrow();
-			break;
-		case 2:User.returnbook();
-			break;
-		case 3:addbook.add();
-			break;
-		case 4: System.exit(1);
-			break;
-		default:System.out.println("please select correct option");
-			break;	
-			
-		}
-		}while(i!=4);
-		s1.close();
-		
-}
-		
-}
-class User {
-    	public static void borrow() {
-        	System.out.println("write book name");
-        	Scanner s2 = new Scanner(System.in);
-        	String j = s2.nextLine();
-        	
-        	if(addbook.searchbook(j)){
-        		System.out.println("book is  available");
-        	}
-        	else{
-        		System.out.println("book is not available");
-        	}
-		s2.close();
-        	// Add logic for borrowing book
-        		
-    		}
-    		
 
-    		public static void returnbook() {
-        	// Add logic for returning book
-        	
-    		}
+class Book{
+    private String name;
+    private String author;
+
+    public Book(String name, String author){
+        this.name = name;
+        this.author = author;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAuthor(){
+        return author;
+    }
 }
-class addbook extends User{
-    		public static void add() {
-        	Library.booklist.add("java");
-        	Library.booklist.add("Probability and Statistics");
-        	System.out.println("Books added successfully");
-    		}
-    		
-    		public static boolean searchbook(String name){
-    			boolean flag = false;
-    			int i=0;
-    			for(String book: Library.booklist){
-    				if(name.equals(book)){
-    					return true;
-    				}
-    				i++;
-    			}
-    			return false;
-    		}
+
+class Library{
+    static ArrayList<Book> booklist = new ArrayList<Book>();
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Library");
+        System.out.println("Please Select the option");
+        System.out.println("1.Borrow book\n2.Return book\n3.Add new book in library\n4.Exit");
+
+        Scanner s1 = new Scanner(System.in);
+        int i;
+
+        do {
+            i = s1.nextInt();
+            s1.nextLine(); // Consume newline character
+            switch (i) {
+                case 1:
+                    borrow.borrowBook();
+                    break;
+                case 2:
+                    User.returnBook();
+                    break;
+                case 3:
+                    addBook.add();
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Please select correct option");
+                    break;
+            }
+        } while (i != 4);
+
+        s1.close();
+    }
 }
+
+class User{
+    public static void returnBook(){
+        // Add logic for returning book
+    }
+}
+
+class borrow extends User{
+    public static void borrowBook(){
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("Search Book name");
+        String name = s2.nextLine();
+
+        if (searchBook(name)){
+            System.out.println("Book is found in Library");
+        } else{
+            System.out.println("Book is not found in library");
+        }
+        System.out.println("1.Students\n2.Faculty");
+        int i = s2.nextInt();
+        if(i==1){
+        	System.out.println("Enter Registration number");
+        	String regno = s2.nextLine();
+        }
+        else if(i==2){
+        	System.out.println("Enter name of faulty");
+        	String naam = s2.nextLine();
+        	System.out.println("enter branch of branch of faculty");
+        	String branch = s2.nextLine();
+        }
+    }
+    
+
+    public static boolean searchBook(String name){
+        for (Book n : Library.booklist) {
+            if (name.equalsIgnoreCase(n.getName())){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class addBook extends User{
+    public static void add(){
+        Scanner s3 = new Scanner(System.in);
+        System.out.println("Enter the name of the book:");
+        String name = s3.nextLine();
+        System.out.println("Enter the author of the book:");
+        String author = s3.nextLine();
+        Library.booklist.add(new Book(name, author));
+        System.out.println("Book added successfully!");
+    }
+}
+
+
 
 /* Algorithm 
 	Library Management system
